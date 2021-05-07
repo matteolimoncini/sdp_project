2fa2ee8ff94b50b62959d474cda9146344e1bf9d
matteolimoncini <matteo@limoncini.it>
2021-05-07 11:47:57 +0200

    removed old endpoints

--- src/main/java/REST/services/DroneService.java
+++ src/main/java/REST/services/DroneService.java
@@ -1,28 +1,23 @@
 package REST.services;
 
-import REST.beans.Dictionary;
 import REST.beans.Drone;
-import REST.beans.Word;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
 @Path("drone")
 public class DroneService {
-    @GET
-    @Produces({"application/json", "application/xml"})
-    public Response getAllWords() {
-        return Response.ok(Dictionary.getInstance()).build();
-    }
 
     @Path("add")
     @POST
     @Consumes({"application/json", "application/xml", "text/plain"})
+    @Produces({"application/json"})
     /*
      Method to add drone into the system with a POST request http.
      Server receive ID, ip address and port number.
 
      If ID is not already used insert drone into the system.
+     If ID is already in use throw exception.
      If insert successfully return drone's list present in the city.
 
     */
@@ -32,17 +27,19 @@ public class DroneService {
         //return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("{\"message\": \"Parola già inserita\"}").build();
     }
 
-    @Path("get/{word}")
-    @GET
-    @Produces({"text/plain"})
-    public Response getWordDefinition(@PathParam("word") String word){
-        return Response.ok().build();
-    }
 
     @Path("delete/{idDrone}")
     @DELETE
     @Consumes({"application/json"})
     @Produces({"application/json"})
+    /*
+     Method to remove drone from the system with a DELETE request http.
+     Server receive drone ID.
+
+     If ID is already used remove drone from the system.
+     If ID not in list throw exception.
+
+    */
     public Response removeDrone(@PathParam("idDrone") Integer idDrone) {
         // TODO implement this
         return Response.ok().build();
