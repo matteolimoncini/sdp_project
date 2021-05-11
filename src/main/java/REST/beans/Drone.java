27e15a0dfa1a6b6ea8b26d8a7b0f0fe65e8e17b7
matteolimoncini <matteo@limoncini.it>
2021-05-11 19:19:58 +0200

    added same methods to drone class and created Dronemain i hope it will be useful

--- src/main/java/REST/beans/Drone.java
+++ src/main/java/REST/beans/Drone.java
@@ -17,7 +17,12 @@ public class Drone {
     private Integer battery;
     private boolean processingDelivery;
 
-    public Drone(){}
+    private MqttClient client;
+    String clientId;
+
+
+    public Drone() {
+    }
 
     public Drone(Integer id, String ipAddress, Integer portNumber) {
         this.id = id;
@@ -39,70 +44,77 @@ public class Drone {
         return portNumber;
     }
 
-    private boolean iAmMaster(){
+    private boolean iAmMaster() {
         return idMaster.equals(id);
     }
 
-    public void subscribe(){
-        if(!iAmMaster()){
+    private boolean iAmProcessingDelivery() {
+        return processingDelivery;
+    }
+
+    private boolean batteryLow(){
+        int lowbatteryThreshold = 15;
+        return this.battery<lowbatteryThreshold;
+    }
+
+    private void quit() throws MqttException {
+        this.disconnect();
+    }
+
+    public void disconnect() throws MqttException {
+        if (this.client.isConnected()) {
+            this.client.disconnect();
+            System.out.println("Subscriber " + this.clientId + " disconnected");
+        }
+        System.out.println("Subscriber already disconnected");
+    }
+
+    public void subscribe() throws MqttException {
+        if (!iAmMaster()) {
             System.err.println("only master can subscribe");
             //throw exception?
         }
-        MqttClient client;
+
         MqttConnectOptions connOpts;
+        clientId = MqttClient.generateClientId();
         String broker = "tcp://localhost:1883";
-        String clientId = MqttClient.generateClientId();
         String topic = "dronazon/smarcity/orders";
         int qos = 2;
 
-        try {
-            client = new MqttClient(broker, clientId);
-            connOpts = new MqttConnectOptions();
-            connOpts.setCleanSession(true);
+        this.client = new MqttClient(broker, clientId);
 
-            // Connect the client
-            System.out.println(clientId + " Connecting Broker " + broker);
-            client.connect(connOpts);
-            System.out.println(clientId + " Connected - Thread PID: " + Thread.currentThread().getId());
+        // Connect the client
+        System.out.println(clientId + " Connecting Broker " + broker);
+        System.out.println(clientId + " Connected - Thread PID: " + Thread.currentThread().getId());
 
-            // Callback
-            client.setCallback(new MqttCallback() {
+        // Callback
+        this.client.setCallback(new MqttCallback() {
 
-                public void messageArrived(String topic, MqttMessage message) {
-                    // Called when a message arrives from the server that matches any subscription made by the client
-                    String time = new Timestamp(System.currentTimeMillis()).toString();
-                    String receivedMessage = new String(message.getPayload());
-                    System.out.println(clientId +" Received a Message! - Callback - Thread PID: " + Thread.currentThread().getId() +
-                            "\n\tTime:    " + time +
-                            "\n\tTopic:   " + topic +
-                            "\n\tMessage: " + receivedMessage +
-                            "\n\tQoS:     " + message.getQos() + "\n");
-                }
+            public void messageArrived(String topic, MqttMessage message) {
+                // Called when a message arrives from the server that matches any subscription made by the client
+                String time = new Timestamp(System.currentTimeMillis()).toString();
+                String receivedMessage = new String(message.getPayload());
+                System.out.println(clientId + " Received a Message! - Callback - Thread PID: " + Thread.currentThread().getId() +
+                        "\n\tTime:    " + time +
+                        "\n\tTopic:   " + topic +
+                        "\n\tMessage: " + receivedMessage +
+                        "\n\tQoS:     " + message.getQos() + "\n");
+            }
 
-                public void connectionLost(Throwable cause) {
-                    System.out.println(clientId + " Connection lost! cause:" + cause.getMessage()+ "-  Thread PID: " + Thread.currentThread().getId());
-                }
+            public void connectionLost(Throwable cause) {
+                System.out.println(clientId + " Connection lost! cause:" + cause.getMessage() + "-  Thread PID: " + Thread.currentThread().getId());
+            }
 
-                public void deliveryComplete(IMqttDeliveryToken token) {
-                    // Not used here
-                }
+            public void deliveryComplete(IMqttDeliveryToken token) {
+                // Not used here
+            }
 
-            });
-            System.out.println(clientId + " Subscribing ... - Thread PID: " + Thread.currentThread().getId());
-            client.subscribe(topic,qos);
-            System.out.println(clientId + " Subscribed to topics : " + topic);
+        });
+        System.out.println(clientId + " Subscribing ... - Thread PID: " + Thread.currentThread().getId());
+        this.client.subscribe(topic, qos);
+        System.out.println(clientId + " Subscribed to topics : " + topic);
 
-            //TODO manageOrder
+        //TODO manageOrder
 
-            client.disconnect();
-
-        } catch (MqttException me ) {
-            System.out.println("reason " + me.getReasonCode());
-            System.out.println("msg " + me.getMessage());
-            System.out.println("loc " + me.getLocalizedMessage());
-            System.out.println("cause " + me.getCause());
-            System.out.println("exception " + me);
-            me.printStackTrace();
-        }
     }
 }
---
+++ src/main/java/REST/beans/DroneMain.java
@@ -0,0 +1,7 @@
+package REST.beans;
+
+public class DroneMain {
+    public static void main(String[] args) {
+
+    }
+}
