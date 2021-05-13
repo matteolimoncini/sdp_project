06b6c030d304253409f661abf920446d71b9a8ef
matteolimoncini <matteo@limoncini.it>
2021-05-13 17:31:50 +0200

    base version of ClientAdmin end

--- src/main/java/REST/ClientAdmin.java
+++ src/main/java/REST/ClientAdmin.java
@@ -14,6 +14,16 @@ import java.util.Scanner;
 public class ClientAdmin {
     public static void main(String[] args) {
         int numberMenu;
+        Scanner in = new Scanner(System.in);
+        Gson gson = new Gson();
+        Client client = Client.create();
+        WebResource webResource;
+        ClientResponse response;
+        MultivaluedMap<String, String> params;
+        String output;
+        String t1;
+        String t2;
+
         while (true) {
             System.out.println("Welcome to the admin page");
             System.out.print("<1> list of drones in the city \n" +
@@ -21,15 +31,9 @@ public class ClientAdmin {
                     "<3> average number of delivery between two timestamp \n" +
                     "<4> average number of kilometers between two timestamp \n" +
                     "press any other key to exit\n");
-            Scanner in = new Scanner(System.in);
+
             numberMenu = in.nextInt();
-            Gson gson = new Gson();
-            Client client = Client.create();
-            WebResource webResource;
-            ClientResponse response;
-            String output;
-            String t1;
-            String t2;
+
 
             switch (numberMenu) {
                 case 1:
@@ -74,7 +78,7 @@ public class ClientAdmin {
                     t1 = in.nextLine();
                     System.out.println("insert t2");
                     t2 = in.nextLine();
-                    MultivaluedMap<String, String> params = new MultivaluedMapImpl();
+                    params = new MultivaluedMapImpl();
                     params.add("t1",t1);
                     params.add("t2",t2);
                     webResource = client.resource("http://localhost:1337/statistics/delivery/avg").queryParams(params);
@@ -95,6 +99,27 @@ public class ClientAdmin {
 
                     break;
                 case 4:
+                    in.nextLine();
+                    System.out.println("insert t1");
+                    t1 = in.nextLine();
+                    System.out.println("insert t2");
+                    t2 = in.nextLine();
+                    params = new MultivaluedMapImpl();
+                    params.add("t1",t1);
+                    params.add("t2",t2);
+                    webResource = client.resource("http://localhost:1337/statistics/kilometers/avg").queryParams(params);
+
+                    response = webResource.accept("application/json")
+                            .get(ClientResponse.class);
+
+                    if (response.getStatus() != 200) {
+                        throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
+                    }
+
+                    output = response.getEntity(String.class);
+                    System.out.println("Output from Server .... \n");
+                    System.out.println(output);
+
                     break;
                 default:
                     System.out.println("exit from menu");
