67dc10d423a88b5440d383b0b74715f6f0bb1e92
matteolimoncini <matteo@limoncini.it>
2021-05-13 17:15:04 +0200

    base version of method 3 of ClientAdmin

--- src/main/java/REST/ClientAdmin.java
+++ src/main/java/REST/ClientAdmin.java
@@ -5,7 +5,9 @@ import com.google.gson.Gson;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
 
+import javax.ws.rs.core.MultivaluedMap;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Scanner;
 @XmlRootElement
@@ -26,6 +28,8 @@ public class ClientAdmin {
             WebResource webResource;
             ClientResponse response;
             String output;
+            String t1;
+            String t2;
 
             switch (numberMenu) {
                 case 1:
@@ -63,11 +67,32 @@ public class ClientAdmin {
                     System.out.println(output);
 
                     //DroneList list = gson.fromJson(output,DroneList.class);
-
-
-
                     break;
                 case 3:
+                    in.nextLine();
+                    System.out.println("insert t1");
+                    t1 = in.nextLine();
+                    System.out.println("insert t2");
+                    t2 = in.nextLine();
+                    MultivaluedMap<String, String> params = new MultivaluedMapImpl();
+                    params.add("t1",t1);
+                    params.add("t2",t2);
+                    webResource = client.resource("http://localhost:1337/statistics/delivery/avg").queryParams(params);
+
+                    response = webResource.accept("application/json")
+                            .get(ClientResponse.class);
+
+                    if (response.getStatus() != 200) {
+                        throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
+                    }
+
+                    output = response.getEntity(String.class);
+                    System.out.println("Output from Server .... \n");
+                    System.out.println(output);
+
+                    //DroneList list = gson.fromJson(output,DroneList.class);
+
+
                     break;
                 case 4:
                     break;
